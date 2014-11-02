670ebf5ddbdeed7aab0a6adb797c3c286ac19179
Hamcha <zikyky@gmail.com>
2014-11-02 16:37:56 +0100

    Different colors, less crazybindings

--- Dodici/Controls/TaskBar.xaml.cs
+++ Dodici/Controls/TaskBar.xaml.cs
@@ -1,20 +1,30 @@
-﻿using Windows.UI.Xaml;
+﻿using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Dodici {
     public sealed partial class TaskBar : UserControl {
         public static DependencyProperty TitleProperty = DependencyProperty.Register("Title",
-            typeof(string), typeof(TaskBar), new PropertyMetadata(false));
+            typeof(string), typeof(TaskBar), new PropertyMetadata(""));
         public static DependencyProperty ProgressProperty = DependencyProperty.Register("Progress",
-            typeof(int), typeof(TaskBar), new PropertyMetadata(false));
+            typeof(double), typeof(TaskBar), new PropertyMetadata(0));
 
         public string Title {
             get { return (string)GetValue(TitleProperty); }
             set { SetValue(TitleProperty, value);  }
         }
-        public int Progress {
-            get { return (int)GetValue(ProgressProperty); }
+        public double Progress {
+            get { return (double)GetValue(ProgressProperty); }
             set { SetValue(ProgressProperty, value); }
+        }
+
+        private Brush BackgroundColor {
+            get {
+                Brush foreground = Foreground;
+                foreground.Opacity = 0.5;
+                return foreground;
+            }
         }
 
         public TaskBar() {
